5e016960385b4e2277804a43aa99c3567698bcf7
yoosername <yoosername@local>
2018-07-31 08:52:14 +0100

    Back to using Servletconfiguration

--- src/main/java/com/example/osgi/framework/App.java
+++ src/main/java/com/example/osgi/framework/App.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @PropertySources({
--- src/main/java/com/example/osgi/framework/web/DispatcherSetup.java
+++ src/main/java/com/example/osgi/framework/web/DispatcherSetup.java
@@ -1,40 +1,71 @@
 package com.example.osgi.framework.web;
 
 import javax.servlet.ServletContext;
-import javax.servlet.ServletRegistration;
+import javax.servlet.ServletException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
-public class DispatcherSetup implements WebApplicationInitializer {
+public class DispatcherSetup extends DispatcherServletAutoConfiguration {
 	
-	private static final Logger dslogger = LoggerFactory.getLogger(DispatcherServlet.class);
-
-    @Override
-    public void onStartup(ServletContext container) {
-    	
-    	OsgiBundleXmlWebApplicationContext appContext = new OsgiBundleXmlWebApplicationContext();
-    	container.addListener(new ContextLoaderListener(appContext));
-
-    	ServletRegistration.Dynamic dispatcher = container.addServlet("dispatcher", new DispatcherServlet(appContext) {
+	private static final Logger logger = LoggerFactory.getLogger(DispatcherSetup.class);
+	
+	@Bean
+	public DispatcherServlet dispatcherServlet() {
+		
+	    return new DispatcherServlet() {
 	    	
 			@Override
 			protected void onRefresh(ApplicationContext context) {
+				// TODO Auto-generated method stub
 				super.onRefresh(context);
-				DispatcherSetup.dslogger.info("SERVLET CONTEXT REFRESHED AS " + context.toString());
+				DispatcherSetup.logger.info("SERVLET CONTEXT REFRESHED AS " + context.toString());
 			}
 	    	
-	    });
-    	
-    	dispatcher.setLoadOnStartup(1);
-    	dispatcher.addMapping("/dispatch");
-    	
-    }
+	    };
+	}
+	
+	@Bean
+	public ServletContextInitializer contextInitializer() {
+	    return new ServletContextInitializer() {
 
- }
+	        @Override
+	        public void onStartup(ServletContext servletContext)
+	                throws ServletException {
+	        	servletContext.setInitParameter("contextClass","com.example.osgi.framework.web.OsgiBundleXmlWebApplicationContext");
+	        	DispatcherSetup.logger.info("NEW SERVLET CONTEXT INIALIZED AS = " + servletContext.toString());
+	        }
+	    };
+	}
+
+	/**
+	 * Register dispatcherServlet programmatically
+	 *
+	 * @return ServletRegistrationBean
+	 */
+	@Bean
+	public ServletRegistrationBean<DispatcherServlet> dispatcherServletRegistration() {
+
+	    ServletRegistrationBean<DispatcherServlet> registration = new ServletRegistrationBean<DispatcherServlet>(
+	            dispatcherServlet(), 
+	            "/dispatch/*"
+	    );
+	    
+	    //registration.addInitParameter("contextClass","com.example.osgi.framework.web.OsgiBundleXmlWebApplicationContext");
+
+	    registration.setName(
+	    		DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_REGISTRATION_BEAN_NAME
+	    );
+
+	    return registration;
+	}
+	
+}
